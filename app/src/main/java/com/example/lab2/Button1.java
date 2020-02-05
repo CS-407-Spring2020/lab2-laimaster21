2c71d2365d10cd2aa0726db3d2e5574015684884
laimaster <cyuan42@wisc.edu>
2020-02-05 21:48:37 +0000

    lab2 feb5

---
+++ app/src/main/java/com/example/lab2/Button1.java
@@ -0,0 +1,10 @@
+package com.example.lab2;
+
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Button1 extends AppCompatActivity {
+    private Button buttonleft;
+    private Button buttonrigt;
+}
