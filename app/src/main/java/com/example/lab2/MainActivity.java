a3d7b8609d29d1be7f90a952f0ae5363dcadc6c6
laimaster <cyuan42@wisc.edu>
2020-02-10 20:08:46 +0000

    lab2 feb10

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction1( View view){
+        Log.i("info","button1 pressed");
+        EditText myTextField=(EditText) findViewById(R.id.editText);
+        Toast.makeText(MainActivity.this,myTextField.getText().toString(),Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
